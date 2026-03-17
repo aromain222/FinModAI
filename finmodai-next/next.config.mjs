317014411fa081c4e0e67ada4f8c74782b2d924b
Avery Romain <averyromain@Averys-MacBook-Air-4.local>
2026-03-17 11:09:21 -0700

    Allow remote news image hosts

--- finmodai-next/next.config.mjs
+++ finmodai-next/next.config.mjs
@@ -8,6 +8,26 @@ if (rootDomain) {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'commons.wikimedia.org',
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       allowedOrigins
